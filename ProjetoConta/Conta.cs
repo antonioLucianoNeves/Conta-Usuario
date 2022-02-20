f56fe7ed2d252e08024ff81e3b4874610675bfb4
antonioLucianoNeves <a_luciano10@hotmail.com>
2022-02-20 12:45:46 -0300

    Conta de usuario

---
+++ ProjetoConta/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProjetoConta
+{
+    class Conta
+    {
+       public int Numero { get; private set; }
+       public string Titular { get; private set; }
+       public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            
+        }
+
+        public Conta (int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta "  
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public void Deposito(double dep)
+        {
+            Saldo += dep;
+        }
+        public void Saque(double saq)
+        {
+            Saldo -= saq + 5.0;
+        }
+    }
+}
---
+++ ProjetoConta/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ProjetoConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre com o número da Conta:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Titular ca conta:");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial?(s/n)");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            conta = new Conta(numero, titular);
+
+            if (depositoInicial == 's' || depositoInicial == 'S')
+            {
+                Console.Write("Entre com o valor do deposito inicial:");
+                double vIni = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, vIni);
+                
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para deposito:");
+            double vDep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da conta atualizados!");
+            conta.Deposito(vDep);
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para Saque:");
+            double vSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da conta atualizados!");
+            conta.Saque(vSaque);
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
